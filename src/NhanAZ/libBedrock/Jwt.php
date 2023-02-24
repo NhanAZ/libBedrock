85e5ffb0ecb4b61c8e8447e120ec704ddac6edce
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2023-02-24 10:32:05 +0700

    Create Jwt.php

---
+++ src/NhanAZ/libBedrock/Jwt.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\libBedrock;
+
+use pocketmine\network\mcpe\JwtUtils;
+
+class Jwt {
+
+	/** @param array{mixed[], mixed[], string} $clientData */
+	public function __construct(private array $clientData) {
+	}
+
+	public static function parse(string $clientDataJwt): self {
+		$clienData = JwtUtils::parse($clientDataJwt);
+		return new self($clienData);
+	}
+
+	public function getalg(): string {
+		return strval($this->clientData[0]["alg"]);
+	}
+}
