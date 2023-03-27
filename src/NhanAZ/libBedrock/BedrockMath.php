fb3f4c4dda221793a293ab03c33f9d1321e9d41d
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2023-03-27 18:39:02 +0700

    Create BedrockMath.php

---
+++ src/NhanAZ/libBedrock/BedrockMath.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\BetterCancel;
+
+use pocketmine\math\Vector3;
+
+class BedrockMath {
+
+	public static function Center(Vector3 $vector3): Vector3 {
+		return $vector3->add(0.5, 0.5, 0.5);
+	}
+}
