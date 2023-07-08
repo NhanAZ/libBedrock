50e5eea24e10f4054cea0226ef9282c4eaabfa9e
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-08 13:02:39 +0700

    Update libBedrock.php

--- src/NhanAZ/libBedrock/libBedrock.php
+++ src/NhanAZ/libBedrock/libBedrock.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace NhanAZ\libBedrock;
 
+use pocketmine\plugin\PluginBase;
+
 /**
  * @package NhanAZ\libBedrock
  *
  * 🌈 A library that provides common features for the PocketMine-MP plugin.
  */
-class libBedrock {
+class libBedrock extends PluginBase {
 }
