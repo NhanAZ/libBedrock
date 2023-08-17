d796391f2f47d35b8855567ab7c418b7330b512e
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-08-18 00:40:00 +0700

    Hot fix: Violation of PSR-0

--- src/NhanAZ/libBedrock/external/thebigcrafter/Hydrogen.php
+++ src/NhanAZ/libBedrock/external/thebigcrafter/Hydrogen.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace NhanAZ\external\thebigcrafter;
+namespace NhanAZ\libBedrock\external\thebigcrafter;
 
 use pocketmine\plugin\Plugin;
 use thebigcrafter\Hydrogen\Hydrogen as thebigcrafterHydrogen;
