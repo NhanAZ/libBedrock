cacaf272e684ec8ff025683d01285108c053a4ca
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-08-18 00:25:10 +0700

    Fix PHPStan Err & Fix CS

--- src/NhanAZ/libBedrock/Counter.php
+++ src/NhanAZ/libBedrock/Counter.php
@@ -27,60 +27,38 @@ class Counter {
 	 * @param string     $dir       The directory to scan.
 	 * @param string     $extension The file extension to filter by.
 	 */
-	public static function getFolderInfo(PluginBase $plugin, string $dir, string $extension, bool $totalFilesT = true, bool $totalLinesT = true, bool $totalWordsT = true, bool $totalCharsT = true) : Async {
+	public static function getFolderInfo(PluginBase $plugin, string $dir, string $extension) : Async {
 		VapmPMMP::init($plugin);
-
 		return new Async(
-			/** @return array{"totalFiles": int, "totalLines": int, "totalWords": int, "totalChars": int} An array of file information. */
-			function () use ($dir, $extension, $totalFilesT, $totalLinesT, $totalWordsT, $totalCharsT) : array {
+			/**
+			 * @return array{"totalFiles": int, "totalLines": int, "totalWords": int, "totalChars": int} An array of file information. */
+			function () use ($dir, $extension) : array {
 				$files = new \RecursiveIteratorIterator(
 					iterator: new \RecursiveDirectoryIterator($dir),
 					mode: \RecursiveIteratorIterator::CHILD_FIRST
 				);
-
 				$txtFiles = [];
-
-				if ($totalFilesT) {
-					foreach ($files as $file) {
-
-						/** @var SplFileInfo $file */
-						if ($file->isFile() && $file->getExtension() == $extension) {
-							$txtFiles[] = $file->getRealPath();
-
-							FiberManager::wait();
-						}
+				foreach ($files as $file) {
+					/** @var SplFileInfo $file */
+					if ($file->isFile() && $file->getExtension() == $extension) {
+						$txtFiles[] = $file->getRealPath();
+						FiberManager::wait();
 					}
 				}
-
 				$totalLines = 0;
 				$totalWords = 0;
 				$totalChars = 0;
-				if ($totalLinesT || $totalWordsT || $totalCharsT) {
-					foreach ($txtFiles as $txtFile) {
-						if ($totalLinesT) {
-
-							$value = file($txtFile);
-							if (is_array($value)) {
-								$totalLines += count($value);
-							}
-						}
-
-						if ($totalWordsT || $totalChars) {
-
-							$content = Async::await(Stream::read($txtFile));
-							if ($content !== Error::UNABLE_TO_OPEN_FILE && is_string($content)) {
-								if ($totalWordsT) {
-									$totalWords += str_word_count($content);
-								}
-
-								if ($totalCharsT) {
-									$totalChars += strlen($content);
-								}
-							}
-						}
+				foreach ($txtFiles as $txtFile) {
+					$value = file($txtFile);
+					if (is_array($value)) {
+						$totalLines += count($value);
+					}
+					$content = Async::await(Stream::read($txtFile));
+					if ($content !== Error::UNABLE_TO_OPEN_FILE && is_string($content)) {
+						$totalWords += str_word_count($content);
+						$totalChars += strlen($content);
 					}
 				}
-
 				return [
 					"totalFiles" => count($txtFiles),
 					"totalLines" => $totalLines,
