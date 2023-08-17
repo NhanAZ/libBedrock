fa332591cd574ca33242d843adc8cd766dbc7c23
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-08-17 22:04:20 +0700

    Add toggle switch for `Counter::getFolderInfo()`

--- src/NhanAZ/libBedrock/Counter.php
+++ src/NhanAZ/libBedrock/Counter.php
@@ -21,30 +21,43 @@ class Counter {
 	 * @param string $extension The file extension to filter by.
 	 * @return array{"totalFiles": int, "totalLines": int, "totalWords": int, "totalChars": int} An array of file information.
 	 */
-	public static function getFolderInfo(string $dir, string $extension) : array {
+	public static function getFolderInfo(string $dir, string $extension, bool $totalFilesT = true, bool $totalLinesT = true, bool $totalWordsT = true, bool $totalCharsT = true): array {
 		$files = new \RecursiveIteratorIterator(
 			iterator: new \RecursiveDirectoryIterator($dir),
 			mode: \RecursiveIteratorIterator::CHILD_FIRST
 		);
 		$txtFiles = [];
-		foreach ($files as $file) {
-			/** @var SplFileInfo $file */
-			if ($file->isFile() && $file->getExtension() == $extension) {
-				$txtFiles[] = $file->getRealPath();
+		if ($totalFilesT) {
+			foreach ($files as $file) {
+				/** @var SplFileInfo $file */
+				if ($file->isFile() && $file->getExtension() == $extension) {
+					$txtFiles[] = $file->getRealPath();
+				}
 			}
 		}
 		$totalLines = 0;
 		$totalWords = 0;
 		$totalChars = 0;
-		foreach ($txtFiles as $txtFile) {
-			$content = file_get_contents($txtFile);
-			$value = file($txtFile);
-			if (is_array($value)) {
-				$totalLines += count($value);
-			}
-			if ($content !== false) {
-				$totalWords += str_word_count($content);
-				$totalChars += strlen($content);
+		if ($totalLinesT || $totalWords || $totalCharsT) {
+			foreach ($txtFiles as $txtFile) {
+				if ($totalLinesT) {
+					$value = file($txtFile);
+					if (is_array($value)) {
+						$totalLines += count($value);
+					}
+				}
+
+				if ($totalWordsT || $totalChars) {
+					$content = file_get_contents($txtFile);
+					if ($content !== false) {
+						if ($totalWordsT) {
+							$totalWords += str_word_count($content);
+						}
+						if ($totalCharsT) {
+							$totalChars += strlen($content);
+						}
+					}
+				}
 			}
 		}
 		/* TODO: Add more info */
